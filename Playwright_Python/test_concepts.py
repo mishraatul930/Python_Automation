d8211595de4e7e8ce8ca71cd027d9f37f37dca26
Atul Mishra <mishraatul930@gmail.com>
2025-12-22 20:06:06 +0530

    Element Click

--- Playwright_Python/test_concepts.py
+++ Playwright_Python/test_concepts.py
@@ -4,6 +4,7 @@ import time
 from playwright.sync_api import Page, expect
 
 def test_navigate(page: Page):
+    page.context.set_viewport_size({"width": 1920, "height": 1080})
     page.goto("https://demoqa.com/")
 
 def test_to_have_title(page: Page):
@@ -13,4 +14,7 @@ def test_to_have_title(page: Page):
 def test_to_click_on_element(page: Page):
     page.goto("https://demoqa.com/")
     page.get_by_role("heading", name="Elements").click()
+    time.sleep(5)
+    # page.get_by_text("Text Box", exact=True).click()
+    page.locator("//span[@class='text' and normalize-space() = 'Text Box']").click()
     time.sleep(5)
