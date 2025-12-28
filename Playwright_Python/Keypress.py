16cf94d6ced57d4adafe4ed42cab6f09c471f9fd
Atul Mishra <mishraatul930@gmail.com>
2025-12-28 12:56:51 +0530

    KeyPress Functionality

---
+++ Playwright_Python/Keypress.py
@@ -0,0 +1,25 @@
+import re
+import time
+
+import pytest
+from playwright.sync_api import Page, expect
+
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page):
+    print("before the test runs")
+
+    # Go to the starting url before each test.
+    page.goto("https://demoqa.com/")
+    yield
+
+    print("after the test runs")
+
+def test_keypress(page: Page):
+    page.get_by_role("heading", name="Elements").click()
+    time.sleep(5)
+    # page.get_by_text("Text Box", exact=True).click()
+    page.locator("//span[@class='text' and normalize-space() = 'Text Box']").click()
+    page.wait_for_timeout(5000)
+    page.locator('#userName').press_sequentially('Atul Mishra')
+    page.wait_for_timeout(5000)
